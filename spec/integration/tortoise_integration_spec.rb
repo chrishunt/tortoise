a631321d07a1d5ddc959f8ae503cd941c21413f8
Chris Hunt <huntca@gmail.com>
2012-02-23 14:34:21 -0800

    Add integration test for html export

--- spec/integration/tortoise_integration_spec.rb
+++ spec/integration/tortoise_integration_spec.rb
@@ -1,10 +1,18 @@
 require 'tortoise'
 
 describe Tortoise::Interpreter do
-  it 'produces the expected ascii output' do
+  before do
     input  = File.new('./spec/data/simple.logo').read
-    output = File.new('./spec/data/simple_out.ascii').read
+    @interpreter = Tortoise::Interpreter.new(input)
+  end
 
-    Tortoise::Interpreter.new(input).to_ascii.should == output
+  it 'produces the expected ascii output' do
+    ascii = File.new('./spec/data/simple_out.ascii').read
+    @interpreter.to_ascii.should == ascii
+  end
+
+  it 'produces the expected html output' do
+    html = File.new('./spec/data/simple_out.html').read
+    @interpreter.to_html.should == html
   end
 end
