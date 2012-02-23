9411a109f37e908d8c84b1776f49e16256736a7c
Chris Hunt <huntca@gmail.com>
2012-02-23 13:57:44 -0800

    Rename spec data file

--- spec/integration/tortoise_integration_spec.rb
+++ spec/integration/tortoise_integration_spec.rb
@@ -3,7 +3,7 @@ require 'tortoise'
 describe Tortoise::Interpreter do
   it 'produces the expected ascii output' do
     input  = File.new('./spec/data/simple.logo').read
-    output = File.new('./spec/data/simple_out.txt').read
+    output = File.new('./spec/data/simple_out.ascii').read
 
     Tortoise::Interpreter.new(input).to_ascii.should == output
   end
